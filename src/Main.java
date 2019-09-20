73f5babd849c36f88ee77afc1d4f5da6fd208935
769879295 <34676072+769879295@users.noreply.github.com>
2019-09-20 15:23:17 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!--->1111");
-        System.out.println("Hello World!--->local 修改");
+        System.out.println("Hello World!--->服务器 修改");
     }
 }
